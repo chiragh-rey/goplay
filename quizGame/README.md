9376b8c9af350c1ee393ac09970566f8d7be9a7a
Chirag Gada <chirag.gada@boku.com>
2019-10-08 17:29:01 +0530

    quizGame: Refactoring

--- quizGame/README.md
+++ quizGame/README.md
@@ -45,3 +45,19 @@ Your quiz should stop as soon as the time limit has exceeded. That is, you shoul
 Users should be asked to press enter (or some other key) before the timer starts, and then the questions should be printed out to the screen one at a time until the user provides an answer. Regardless of whether the answer is correct or wrong the next question should be asked.
 
 At the end of the quiz the program should still output the total number of questions correct and how many questions there were in total. Questions given invalid answers or unanswered are considered incorrect.
+
+### How to Run
+```
+# Commands to be executed from quizGame folder
+# Build command
+go build -o goexecutable
+
+# Help command
+./goexecutable -h
+
+# Run with default values
+./goexecutable
+
+# Run with arguments
+./goexecutable -csv=myproblems.csv -limit=60
+```
--- quizGame/main.go
+++ quizGame/main.go
@@ -12,14 +12,25 @@ import (
 // Compile and Execute: go build -o goexecutable && ./goexecutable
 // Get help and parameter details: go build . && ./goexecutable -h
 
+type problem struct {
+	question string
+	answer string
+}
+
 func main()  {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	timeLimit := flag.Int("limit", 30, "the time limit for quiz in seconds")
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename)
+	problems := processFile(*csvFilename)
+
+	startTest(problems, *timeLimit)
+}
+
+func processFile(csvFilename string) []problem {
+	file, err := os.Open(csvFilename)
 	if err != nil {
-		exit(err, fmt.Sprintf("Failed to open the CSV File: %s", *csvFilename))
+		exit(err, fmt.Sprintf("Failed to open the CSV File: %s", csvFilename))
 	}
 
 	r := csv.NewReader(file)
@@ -28,32 +39,7 @@ func main()  {
 		exit(err, "Failed to parse provided CSV file")
 	}
 
-	problems := parseLines(lines)
-	var answer string
-	correct := 0
-	answerCh := make(chan string)
-	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-
-		go func() {
-			fmt.Scanf("%s\n", &answer)
-			answerCh <- answer
-		}()
-
-		select {
-			case <-timer.C:
-				fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-				return
-			case answer := <-answerCh:
-				if answer == p.a {
-					correct++
-				}
-		}
-	}
-
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+	return parseLines(lines)
 }
 
 func parseLines(lines [][]string) []problem {
@@ -61,17 +47,40 @@ func parseLines(lines [][]string) []problem {
 
 	for i, line := range lines {
 		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
+			question: line[0],
+			answer: strings.TrimSpace(line[1]),
 		}
 	}
 
 	return ret
 }
 
-type problem struct {
-	q string
-	a string
+func startTest(problems []problem, timeLimit int) {
+	var answer string
+	correct := 0
+	answerCh := make(chan string)
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+
+		go func() {
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.answer {
+				correct++
+			}
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func exit(err error, msg string) {
